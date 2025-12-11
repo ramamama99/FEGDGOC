d691bcf0c8d544bd55d61362627b5ab3e3ff8a84
unknown <rama99>
2025-12-11 13:12:54 +0700

    feat : age logic

---
+++ main.js
@@ -0,0 +1,9 @@
+const input = require('prompt-sync')({ sigint: true})
+const nama = input("Siapa Namamu?: ");
+const umur = input("Berapa usia mu?: ");
+if (umur < 21){
+    let kurang = 21-umur;
+    console.log(`Halo ${nama},maaf umur kamu kurang ${kurang} tahun lagi`);
+}else{
+    const uang = input("Berapa uangmu?: ");
+}
